a56d631897b853062a25de49fe93949558d4fcc2
hsbaewa <hsbaewa@gmail.com>
2017-01-20 10:38:03 +0900

    최초 등록

---
+++ app/src/main/java/kr/co/hs/icon/sample/SampleActivity.java
@@ -0,0 +1,12 @@
+package kr.co.hs.icon.sample;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 생성된 시간 2017-01-20, Bae 에 의해 생성됨
+ * 프로젝트 이름 : IconResource
+ * 패키지명 : kr.co.hs.icon.sample
+ */
+
+public class SampleActivity extends AppCompatActivity {
+}
